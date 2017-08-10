06fcaec6c43be074d0aeaa6ab89d3c15561dcc09
EdwinDian <hyeaquila@live.co.uk>
2017-08-10 14:19:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
 
 	var key = "0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18";
 	var url = 'https://photorankapi-a.akamaihd.net/?auth_token=';
-	var vrsn = '&version=v2.2';
 
-	url += key + vrsn;
+	url += key;
 
 	$.ajax({
 
